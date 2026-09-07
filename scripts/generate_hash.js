0d9953a5c0d25219bc0869a6f219d245ea211be9
tudam1699 <151566949+tudam1699@users.noreply.github.com>
2026-09-07 23:30:17 +0500

    Update generate_hash.js

--- scripts/generate_hash.js
+++ scripts/generate_hash.js
@@ -1,1 +1,17 @@
+const bcrypt = require('bcryptjs');
+
+const password = process.argv[2];
+
+if (!password) {
+  console.log('Usage: node scripts/generate_hash.js <password>');
+  process.exit(1);
+}
+
+const saltRounds = 12;
+bcrypt.hash(password, saltRounds, (err, hash) => {
+  if (err) throw err;
+  console.log('\n--- YOUR ADMIN HASH ---');
+  console.log(hash);
+  console.log('-----------------------\n');
+});
 
